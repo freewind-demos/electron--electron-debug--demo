59f5092d2bfa01cd14c16e00114074cb706dd83a
peng.li <nowindlee@gmail.com>
2019-08-18 19:49:11 +0800

    add electron-debug by default

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,9 @@
 const electron = require('electron');
+const electronDebug = require('electron-debug');
 
 const {app, BrowserWindow} = electron;
+
+electronDebug();
 
 app.on('ready', function () {
     const mainWindow = new BrowserWindow();
